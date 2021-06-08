23c70e72066bbc47652a9dc91f19120650a64ff7
sonallux <13821543+sonallux@users.noreply.github.com>
2021-06-08 14:39:23 +0200

    Add module-info.java files

---
+++ spotify-web-api-java-generator/src/moditect/module-info.java
@@ -0,0 +1,14 @@
+module de.sonallux.spotify.generator.java {
+    requires com.google.common;
+    requires flexmark;
+    requires flexmark.util.ast;
+    requires flexmark.util.data;
+    requires info.picocli;
+    requires org.slf4j;
+    requires com.github.mustachejava;
+    requires spotify.web.api.core;
+
+    exports de.sonallux.spotify.generator.java;
+    exports de.sonallux.spotify.generator.java.templates;
+    exports de.sonallux.spotify.generator.java.util;
+}

bbd30e5b74d56b5f4ac74785afd5a7c430198a4c
Jonas <13821543+sonallux@users.noreply.github.com>
2021-06-18 11:19:27 +0200

    Add module-info.java files (#66)

---
+++ spotify-web-api-java/src/moditect/module-info.java
@@ -0,0 +1,32 @@
+module de.sonallux.spotify.api {
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires okio;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires okhttp3;
+
+    exports de.sonallux.spotify.api;
+    exports de.sonallux.spotify.api.apis;
+    exports de.sonallux.spotify.api.apis.albums;
+    exports de.sonallux.spotify.api.apis.artists;
+    exports de.sonallux.spotify.api.apis.browse;
+    exports de.sonallux.spotify.api.apis.episodes;
+    exports de.sonallux.spotify.api.apis.follow;
+    exports de.sonallux.spotify.api.apis.library;
+    exports de.sonallux.spotify.api.apis.markets;
+    exports de.sonallux.spotify.api.apis.personalization;
+    exports de.sonallux.spotify.api.apis.player;
+    exports de.sonallux.spotify.api.apis.playlists;
+    exports de.sonallux.spotify.api.apis.search;
+    exports de.sonallux.spotify.api.apis.shows;
+    exports de.sonallux.spotify.api.apis.tracks;
+    exports de.sonallux.spotify.api.apis.usersprofile;
+    exports de.sonallux.spotify.api.authorization;
+    exports de.sonallux.spotify.api.authorization.authorization_code;
+    exports de.sonallux.spotify.api.authorization.client_credentials;
+    exports de.sonallux.spotify.api.authorization.implicit_grant;
+    exports de.sonallux.spotify.api.http;
+    exports de.sonallux.spotify.api.models;
+    exports de.sonallux.spotify.api.util;
+}

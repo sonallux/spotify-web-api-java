fe454643962aee8ca1684672021aeedf08fb2735
sonallux <13821543+sonallux@users.noreply.github.com>
2024-11-02 07:56:48 +0000

    Automated update of fixed-spotify-open-api.yml

--- spotify-web-api-java/src/main/generated/de/sonallux/spotify/api/apis/TracksApi.java
+++ spotify-web-api-java/src/main/generated/de/sonallux/spotify/api/apis/TracksApi.java
@@ -63,7 +63,7 @@ public class TracksApi {
     /**
      * <h4>Get Several Tracks</h4>
      * <p>Get Spotify catalog information for multiple tracks based on their Spotify IDs.</p>
-     * @param ids <p>A comma-separated list of the <a href="https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids">Spotify IDs</a>. For example: <code>ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M</code>. Maximum: 100 IDs.</p>
+     * @param ids <p>A comma-separated list of the <a href="https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids">Spotify IDs</a>. For example: <code>ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M</code>. Maximum: 50 IDs.</p>
      * @return a {@link GetSeveralTracksRequest} object to build and execute the request
      */
     public GetSeveralTracksRequest getSeveralTracks(String ids) {
